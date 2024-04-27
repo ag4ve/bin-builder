a5317924ee7448c0648b0b061c58ee3c0360226e
Your Name <you@example.com>
2024-04-27 04:55:23 -0400

    Use envsubst as a type of template generator and add a function for it called tmplfile. Delete individual Dockerfile.in files since we are using template files in includes/. Move rust_git to rust_ytermusic since this build is only going to be for that one app and nothing else from a git repo.

--- includes/build.mk
+++ includes/build.mk
@@ -10,21 +10,35 @@ ASSETS_FULL := $(shell cat assets.txt)
 FULL_IMAGE := $(notdir $(CURDIR))
 IMAGE := $(word 1, $(subst _, ,$(FULL_IMAGE)))
 
-.PHONY: all
-.SECONDEXPANSION:
-all: docker-build $$(ASSETS)
-	echo $(ASSETS)
+# Use exported variables to fill in a template file and produce an output file
+# Make does not display line breaks so this produces no output
+# $(call <template filename>, <output filename>, <variable name>
+# variable example is not passed to bash directory, but is separated by ';' and
+# '\' to add spaces. So:
+# TMPL_VAR = foo=bar ; baz=bam\bam2
+# Passes foo="bar" and baz="bam bam2"
+define tmplfile
+	$(foreach export_var, \
+		$(subst ;, , $(subst  ,,$(3))), \
+		$(eval export $(subst \, , $(export_var))) \
+		$(eval TMPLFILE_VAR += $$$$$(firstword $(subst =, , $(export_var)))) \
+	)
+	cat $(1) | envsubst '$(TMPLFILE_VAR)' > $(2)
+	$(eval undefine TMPLFILE_VAR)
+endef
 
-Dockerfile:
-	cat Dockerfile.in ../includes/Dockerfile.part > Dockerfile
+# Variable is named after the Dockerfile. So, Dockerfile.base uses TMPL_base as
+# the variable
+Dockerfile.%:
+	$(call tmplfile, ../includes/$@, $@, $(TMPL_$(lastword $(subst ., , $@)))) \
 
-.PHONY: docker-build
-docker-build: Dockerfile
+.PHONY: docker-base
+docker-base: Dockerfile.base
 	docker build \
 		--no-cache \
 		--build-arg UID=$(UID) \
 		--build-arg GID=$(GID) \
 		--build-arg UNAME=$(UNAME) \
 		--tag $(REGISTRY)/$(IMAGE) \
-		.
+		-f ./$^ .
  
